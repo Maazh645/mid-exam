9677a0a2cf3b440be6a5842cb3266945d3127ea6
Maaz <maazh645@gmail.com>
2024-11-01 11:36:36 +0500

    First commit

---
+++ app/tabs/home.jsx
@@ -0,0 +1,28 @@
+import { Stack } from 'expo-router/stack'
+
+export default function Layout() {
+  return <Stack />
+}
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
+
+export default function Page() {
+  const { user } = useUser()
+
+  return (
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/sign-in">
+          <Text>Sign In</Text>
+        </Link>
+        <Link href="/sign-up">
+          <Text>Sign Up</Text>
+        </Link>
+      </SignedOut>
+    </View>
+  )
+}
---
+++ components/Customfield.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Customfield = () => {
+  return (
+    <View>
+      <Text>Customfield</Text>
+    </View>
+  )
+}
+
+export default Customfield
